cef3c3d2f0e9a2322430a7c662e858214f89378e
kaduFranca <kadugabrielmdf@gmail.com>
2025-04-28 22:33:36 -0300

    mockado links

--- supabase/client.js
+++ supabase/client.js
@@ -2,8 +2,8 @@
 
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://rlvblksgyvczdtxctspj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdmJsa3NneXZjemR0eGN0c3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4ODM3NTgsImV4cCI6MjA2MTQ1OTc1OH0.WXaYDOygQpNo9qIzuZLN9LMMuhKISmhLZ5tAOEhCnCI';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
