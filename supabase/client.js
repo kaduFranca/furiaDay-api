fc126fb228af1d78d7daa06f1b8bb111c930866f
kaduFranca <kadugabrielmdf@gmail.com>
2025-04-28 22:51:16 -0300

    corrigido secret

--- supabase/client.js
+++ supabase/client.js
@@ -3,7 +3,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://rlvblksgyvczdtxctspj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdmJsa3NneXZjemR0eGN0c3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4ODM3NTgsImV4cCI6MjA2MTQ1OTc1OH0.WXaYDOygQpNo9qIzuZLN9LMMuhKISmhLZ5tAOEhCnCI';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdmJsa3NneXZjemR0eGN0c3BqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTg4Mzc1OCwiZXhwIjoyMDYxNDU5NzU4fQ.34ORfCD6xjoNW1PnjHyIU8sZrGpoW9c6eZkvZLCU4OE';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
