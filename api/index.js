5b07437cfb66b6bf3d92feea1a305b8270be9960
kaduFranca <kadugabrielmdf@gmail.com>
2025-05-04 13:29:15 -0300

    Removida duplicação na configuração das rotas de usuários no arquivo index.js, mantendo a estrutura de rotas organizada e clara.

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const liquipediaRouter = require('../routes/liquipediaRoutes');
 const userRouter = require('../routes/userRoutes');
 
 app.use(express.json());
+
+// Registra as rotas
+app.use('/users', userRouter);
 app.use('/messages', messagesRouter);
 app.use('/liquipedia', liquipediaRouter);
-app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
   res.send('1,2,3... FURIA!!!');
