cfb276a99e55cb844d55b71dfca49bce18bcbc90
kaduFranca <kadugabrielmdf@gmail.com>
2025-05-04 14:17:55 -0300

    Adicionada configuração CORS no arquivo index.js para permitir requisições de diferentes origens, melhorando a interoperabilidade da API.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,14 @@ const messagesRouter = require('../routes/messages');
 const liquipediaRouter = require('../routes/liquipediaRoutes');
 const userRouter = require('../routes/userRoutes');
 
+// Configuração CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(express.json());
 
 // Registra as rotas
